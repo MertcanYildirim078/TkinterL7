d572e844b3f4601772093f23279d8fad3a798420
MertcanYildirim078 <99068314@mydavinci.nl>
2022-02-15 10:00:13 +0100

    'Lightswitchdone'

--- lightswitch.py
+++ lightswitch.py
@@ -1,13 +1,32 @@
 import tkinter as tk
 window = tk.Tk()
-
-button = tk.Button(text='...', bg="white", fg="black")
+button = tk.Button(text='Lightswitch OFF', bg="white", fg="black")
 button.pack(pady = 20, padx = 20)
 
 # schijf hier tussen je code
-
-
+# def Buttonclick():
+    # process = True
+    # if tk.Button(bg="white"):                                  #if window['background'] == 'white':
+    #     tk.Button(text='lightswitch ON')
+    #     print('Lightswitch ON')
+    #     tk.Button(bg='black')
+    # elif tk.Button(bg='black'):
+    #     tk.Button(bg='black')
+    #     tk.Button( tk.Button(text='lightswitch OFF'))
+    #     print('Lightswitch OFF')
+    #     tk.Button(bg='white')
+def Buttonclick():
+    if window['background'] == 'white':
+        window['background'] = 'black'
+    else:
+        window['background'] = 'white'
+    if window['background'] == 'white':
+        button.config(text='Lightswitch ON')    #config == veranderen, command == iets gebeurd, doet of aanklikt.
+        print('Lightswitch ON')
+    elif window['background'] == 'black':
+        button.config(text='Lightswitch OFF')
+        print('Lightswitch OFF')
 
 # schijf hier tussen je code
-
+button.config(command=Buttonclick)
 window.mainloop()
